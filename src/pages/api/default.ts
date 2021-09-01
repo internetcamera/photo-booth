dd09f435d58d591c8b4820168514deeb7a25fe92
CJ Pais <cj@cjpais.com>
2021-09-01 14:19:44 -0700

    delete server and api folders, resolve tsc warnings

--- src/pages/api/default.ts
+++
@@ -1,8 +0,0 @@
-import APIHandler from '@server/middlewares/APIHandler';
-import { NextApiHandler } from 'next';
-
-const api: NextApiHandler = async (req, res) => {
-  return res.json({});
-};
-
-export default APIHandler(api);
